733bd964b9b9bc844095175f7e6183c98b7e1c96
razibrahim1 <65858033+razibrahim1@users.noreply.github.com>
2023-09-27 10:39:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 // Configure CORS to allow requests from your frontend domain
 app.use(
   cors({
-    origin: "https://pinvent-devqs0rq5-razi-ibrahims-projects.vercel.app", // Replace with your frontend URL
+    origin: "https://pinvent-app-front.vercel.app/", // Replace with your frontend URL
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // Enable passing cookies, if needed
   })
