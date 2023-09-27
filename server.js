b141768345cb219fcdd50afbdd84779d5e0bc168
razibrahim1 <65858033+razibrahim1@users.noreply.github.com>
2023-09-27 09:01:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors"); // Import the 'cors' middleware
+const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const contactRoute = require("./routes/contactRoute");
@@ -12,27 +12,21 @@ const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
 
-// Configure CORS to allow requests from your frontend domain
-const corsOptions = {
-  origin: "https://pinvent-app-front.vercel.app", // Replace with your frontend URL
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Enable passing cookies, if needed
-};
-
-app.use(cors(corsOptions));
-
-// Serve static files from the "uploads" directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Routes middleware
+// Routes Middleware
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/contactus", contactRoute);
@@ -44,8 +38,8 @@ app.get("/", (req, res) => {
 
 // Error Middleware
 app.use(errorHandler);
-
 // Connect to DB and start server
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
